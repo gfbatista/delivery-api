48689c0cc4a4d16abba619ad099005bafd933010
gfbatista <gilbertoferrari@gmail.com>
2024-01-08 23:28:33 -0300

    chore: adding address doc

--- prisma/migrations/20240109022337_deliveryman_migration/migration.sql
+++ prisma/migrations/20240109022337_deliveryman_migration/migration.sql
@@ -31,7 +31,7 @@ CREATE TABLE `categories` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `adresses` (
+CREATE TABLE `addresses` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `uuid` VARCHAR(191) NOT NULL,
     `user_id` INTEGER NOT NULL,
@@ -43,13 +43,37 @@ CREATE TABLE `adresses` (
     `zipcode` VARCHAR(191) NULL,
     `latitude` VARCHAR(191) NOT NULL,
     `longitude` VARCHAR(191) NOT NULL,
+    `primary` BOOLEAN NOT NULL DEFAULT true,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `deleted_at` DATETIME(3) NULL,
 
-    UNIQUE INDEX `adresses_uuid_key`(`uuid`),
+    UNIQUE INDEX `addresses_uuid_key`(`uuid`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `deliverymen` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `uuid` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `drivers_license` VARCHAR(191) NOT NULL,
+    `company` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `street` VARCHAR(191) NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `district` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `number` INTEGER NULL,
+    `zipcode` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `deleted_at` DATETIME(3) NULL,
+
+    UNIQUE INDEX `deliverymen_uuid_key`(`uuid`),
+    UNIQUE INDEX `deliverymen_drivers_license_key`(`drivers_license`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `adresses` ADD CONSTRAINT `adresses_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `addresses` ADD CONSTRAINT `addresses_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
