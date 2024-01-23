d57615aa42f54dbcb33ba2ea118c8cc86d8f4e0a
gfbatista <gilbertoferrari@gmail.com>
2024-01-22 21:29:16 -0300

    chore: add sonar config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,12 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [tsconfigPaths()],
+    test: {
+        coverage: {
+            reporter: ['lcov', 'html'],
+            exclude: [
+                '**/*.spec.ts',
+            ],
+        }
+    }
 });
